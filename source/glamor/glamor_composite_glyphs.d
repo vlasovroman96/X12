dea00aa78bc206335ec411db80860419f90441f3
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-05-22 10:28:48 +0700

    Fixed glamor.glamor_composite_glyphs

--- source/glamor/glamor_composite_glyphs.d
+++ source/glamor/glamor_composite_glyphs.d
@@ -182,13 +182,13 @@ private Bool glamor_glyph_add(glamor_glyph_atlas* atlas, DrawablePtr glyph_draw)
 
 private const(glamor_facet) glamor_facet_composite_glyphs_es300 = {
     name: "composite_glyphs",
-    version: 130,
+    c_version: 130,
     fs_extensions: ("#extension GL_EXT_blend_func_extended : enable\n"),
     vs_vars: ("in vec4 primitive;\n"
                 ~ "in vec2 source;\n"
                 ~ "out vec2 glyph_pos;\n"),
     vs_exec: ("       vec2 pos = primitive.zw * vec2(gl_VertexID&1, (gl_VertexID&2)>>1);\n"
-                GLAMOR_POS(gl_Position, (primitive.xy + pos))
+                ~GLAMOR_POS(gl_Position, (primitive.xy + pos))~
                 "       glyph_pos = (source + pos) * ATLAS_DIM_INV;\n"),
     fs_vars: ("in vec2 glyph_pos;\n"
                 ~ "out vec4 color0;\n"
@@ -200,12 +200,12 @@ private const(glamor_facet) glamor_facet_composite_glyphs_es300 = {
 
 private const(glamor_facet) glamor_facet_composite_glyphs_130 = {
     name: "composite_glyphs",
-    version: 130,
+    c_version: 130,
     vs_vars: ("in vec4 primitive;\n"
                 ~ "in vec2 source;\n"
                 ~ "out vec2 glyph_pos;\n"),
     vs_exec: ("       vec2 pos = primitive.zw * vec2(gl_VertexID&1, (gl_VertexID&2)>>1);\n"
-                GLAMOR_POS(gl_Position, (primitive.xy + pos))
+                ~GLAMOR_POS(gl_Position, (primitive.xy + pos))~
                 "       glyph_pos = (source + pos) * ATLAS_DIM_INV;\n"),
     fs_vars: ("in vec2 glyph_pos;\n"
                 ~ "out vec4 color0;\n"
@@ -221,7 +221,7 @@ private const(glamor_facet) glamor_facet_composite_glyphs_120 = {
                 ~ "attribute vec2 source;\n"
                 ~ "varying vec2 glyph_pos;\n"),
     vs_exec: ("       vec2 pos = vec2(0,0);\n"
-                GLAMOR_POS(gl_Position, primitive.xy)
+                ~GLAMOR_POS(gl_Position, primitive.xy)~
                 "       glyph_pos = source.xy * ATLAS_DIM_INV;\n"),
     fs_vars: ("varying vec2 glyph_pos;\n"),
     fs_exec: ("       vec4 mask = texture2D(atlas, glyph_pos);\n"),
@@ -231,13 +231,13 @@ private const(glamor_facet) glamor_facet_composite_glyphs_120 = {
 
 private const(glamor_facet) glamor_facet_composite_glyphs_gles2 = {
     name: "composite_glyphs",
-    version: 100,
+    c_version: 100,
     fs_extensions: ("#extension GL_EXT_blend_func_extended : enable\n"),
     vs_vars: ("attribute vec2 primitive;\n"
                 ~ "attribute vec2 source;\n"
                 ~ "varying vec2 glyph_pos;\n"),
     vs_exec: ("       vec2 pos = vec2(0,0);\n"
-                GLAMOR_POS(gl_Position, primitive.xy)
+                ~GLAMOR_POS(gl_Position, primitive.xy)~
                 "       glyph_pos = source.xy * ATLAS_DIM_INV;\n"),
     fs_vars: ("varying vec2 glyph_pos;\n"),
     fs_exec: ("       vec4 mask = texture2D(atlas, glyph_pos);\n"),
@@ -284,7 +284,7 @@ private void glamor_glyphs_flush(CARD8 op, PicturePtr src, PicturePtr dst, glamo
 
         BUG_RETURN(!pixmap_priv);
 
-        glamor_pixmap_loop(pixmap_priv, box_index) {
+        glamor_pixmap_loop(pixmap_priv, box_index); {
             BoxPtr box = RegionRects(dst.pCompositeClip);
             int nbox = RegionNumRects(dst.pCompositeClip);
 
