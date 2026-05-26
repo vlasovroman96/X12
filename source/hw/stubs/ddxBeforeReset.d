66eaf41d72245518e771d6a364ee1c55005e04d2
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-05-26 22:30:38 +0700

    Fixed hw.stub.*

---
+++ source/hw/stubs/ddxBeforeReset.d
@@ -0,0 +1,14 @@
+module ddxBeforeReset.c;
+@nogc nothrow:
+extern(C): __gshared:
+/* SPDX-License-Identifier: MIT OR X11
+ *
+ * Copyright © 2024 Enrico Weigelt, metux IT consult <info@metux.net>
+ */
+import dix-config;
+
+import os.ddx_priv;
+
+void ddxBeforeReset()
+{
+}
