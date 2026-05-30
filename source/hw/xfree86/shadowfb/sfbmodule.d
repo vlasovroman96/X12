f3d4355511f854ed6612f75b5782bc592dfc3d1e
vlasovroman96 <vlasovroman.ru@yandex.ru>
2026-05-30 10:18:46 +0700

    Fixed hw.xfee86.shadowfb.*

---
+++ source/hw/xfree86/shadowfb/sfbmodule.d
@@ -0,0 +1,23 @@
+module hw.xfree86.shadowfb.sfbmodule;
+@nogc nothrow:
+extern(C): __gshared:
+import xorg_config;
+
+import xf86Module;
+
+private XF86ModuleVersionInfo VersRec = {
+    modname: "shadowfb",
+    vendor: MODULEVENDORSTRING,
+    _modinfo1_: MODINFOSTRING1,
+    _modinfo2_: MODINFOSTRING2,
+    xf86version: XORG_VERSION_CURRENT,
+    majorversion: 1,
+    minorversion: 0,
+    patchlevel: 0,
+    abiclass: ABI_CLASS_ANSIC,
+    abiversion: ABI_ANSIC_VERSION,
+};
+
+export XF86ModuleData shadowfbModuleData = {
+    vers: &VersRec
+};
---
+++ source/hw/xfree86/shadowfb/shadowfb.d
@@ -0,0 +1,165 @@
+module shadowfb.c;
+@nogc nothrow:
+extern(C): __gshared:
+/*
+   Copyright (C) 1999.  The XFree86 Project Inc.
+   Copyright 2014 Red Hat, Inc.
+
+   Written by Mark Vojkovich (mvojkovi@ucsd.edu)
+   Pre-fb-write callbacks and RENDER support - Nolan Leake (nolan@vmware.com)
+*/
+import xorg_config;
+
+import X11.X;
+import X11.Xproto;
+import X11.fonts.font;
+import X11.fonts.fontstruct;
+
+import dix.screen_hooks_priv;
+
+import misc;
+import pixmapstr;
+import input;
+import mi;
+import scrnintstr;
+import windowstr;
+import gcstruct;
+import dixfontstr;
+import xf86;
+import xf86str;
+import shadowfb;
+
+import picturestr;
+
+
+
+
+struct _ShadowScreenRec {
+    ScrnInfoPtr pScrn;
+    RefreshAreaFuncPtr preRefresh;
+    RefreshAreaFuncPtr postRefresh;
+    CreateWindowProcPtr CreateWindow;
+}alias ShadowScreenRec = _ShadowScreenRec;
+alias ShadowScreenPtr = ShadowScreenRec*;
+
+private DevPrivateKeyRec ShadowScreenKeyRec;
+
+private ShadowScreenPtr shadowfbGetScreenPrivate(ScreenPtr pScreen)
+{
+    return dixLookupPrivate(&(pScreen).devPrivates, &ShadowScreenKeyRec);
+}
+
+Bool ShadowFBInit2(ScreenPtr pScreen, RefreshAreaFuncPtr preRefreshArea, RefreshAreaFuncPtr postRefreshArea)
+{
+    ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
+    ShadowScreenPtr pPriv = void;
+
+    if (!preRefreshArea && !postRefreshArea)
+        return FALSE;
+
+    if (!dixRegisterPrivateKey(&ShadowScreenKeyRec, PRIVATE_SCREEN, 0))
+        return FALSE;
+
+    if (((pPriv = cast(ShadowScreenPtr) calloc(1, ShadowScreenRec.sizeof)) == 0))
+        return FALSE;
+
+    dixSetPrivate(&pScreen.devPrivates, &ShadowScreenKeyRec, pPriv);
+
+    pPriv.pScrn = pScrn;
+    pPriv.preRefresh = preRefreshArea;
+    pPriv.postRefresh = postRefreshArea;
+
+    dixScreenHookClose(pScreen, ShadowCloseScreen);
+
+    pPriv.CreateWindow = pScreen.CreateWindow;
+    pScreen.CreateWindow = ShadowCreateRootWindow;
+
+    return TRUE;
+}
+
+Bool ShadowFBInit(ScreenPtr pScreen, RefreshAreaFuncPtr refreshArea)
+{
+    return ShadowFBInit2(pScreen, null, refreshArea);
+}
+
+/*
+ * Note that we don't do DamageEmpty, or indeed look at the region inside the
+ * DamagePtr at all.  This is an optimization, believe it or not.  The
+ * incoming RegionPtr is the new damage, and if we were to empty the region
+ * miext/damage would just have to waste time reallocating and re-unioning
+ * it every time, whereas if we leave it around the union gets fast-pathed
+ * away.
+ */
+
+private void shadowfbReportPre(DamagePtr damage, RegionPtr reg, void* closure)
+{
+    ShadowScreenPtr pPriv = closure;
+
+    if (!pPriv.pScrn.vtSema)
+        return;
+
+    pPriv.preRefresh(pPriv.pScrn, RegionNumRects(reg), RegionRects(reg));
+}
+
+private void shadowfbReportPost(DamagePtr damage, RegionPtr reg, void* closure)
+{
+    ShadowScreenPtr pPriv = closure;
+
+    if (!pPriv.pScrn.vtSema)
+        return;
+
+    pPriv.postRefresh(pPriv.pScrn, RegionNumRects(reg), RegionRects(reg));
+}
+
+private Bool ShadowCreateRootWindow(WindowPtr pWin)
+{
+    Bool ret = void;
+    ScreenPtr pScreen = pWin.drawable.pScreen;
+    ShadowScreenPtr pPriv = shadowfbGetScreenPrivate(pScreen);
+
+    /* paranoia */
+    if (pWin != pScreen.root)
+        ErrorF("ShadowCreateRootWindow called unexpectedly\n");
+
+    /* call down, but don't hook ourselves back in; we know the first time
+     * we're called it's for the root window.
+     */
+    pScreen.CreateWindow = pPriv.CreateWindow;
+    ret = pScreen.CreateWindow(pWin);
+
+    /* this might look like it leaks, but the damage code reaps listeners
+     * when their drawable disappears.
+     */
+    if (ret) {
+        DamagePtr damage = void;
+
+        if (pPriv.preRefresh) {
+            damage = DamageCreate(&shadowfbReportPre, null,
+                                  DamageReportRawRegion,
+                                  TRUE, pScreen, pPriv);
+            DamageRegister(&pWin.drawable, damage);
+        }
+
+        if (pPriv.postRefresh) {
+            damage = DamageCreate(&shadowfbReportPost, null,
+                                  DamageReportRawRegion,
+                                  TRUE, pScreen, pPriv);
+            DamageSetReportAfterOp(damage, TRUE);
+            DamageRegister(&pWin.drawable, damage);
+        }
+    }
+
+    return ret;
+}
+
+private void ShadowCloseScreen(CallbackListPtr* pcbl, ScreenPtr pScreen, void* unused)
+{
+    dixScreenUnhookClose(pScreen, ShadowCloseScreen);
+
+    ShadowScreenPtr pPriv = shadowfbGetScreenPrivate(pScreen);
+    if (!pPriv)
+        return;
+
+    free(pPriv);
+    dixSetPrivate(&pScreen.devPrivates, &ShadowScreenKeyRec, null);
+}
